f153c0942ec1b0b3a03d320c10fc4bab22faeee3
Егор Романович <Egor_rom13@mail.ru>
2025-09-23 13:16:46 +0500

    Add project files.

---
+++ Tyuiu.RomanovichEN.Sprint1.Task0.V30.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.RomanovichEN.Sprint1.Task0.V30.Lib
+{
+    public class DataService : ISprint1Task0V30
+    {
+        public double Calculate()
+        {
+            return 20 * 5 - 4;
+        }
+    }
+}
---
+++ Tyuiu.RomanovichEN.Sprint1.Task0.V30.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.RomanovichEN.Sprint1.Task0.V30.Lib;
+namespace Tyuiu.RomanovichEN.Sprint1.Task0.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(96, res);
+        }
+    }
+}
